1043199083f215df639cee607b59d83b31e1f90c
Dan Clark <dannyclark_90@hotmail.co.uk>
2025-07-22 14:48:21 +0100

    V-bind and shorthand implemented to anchor tag and product image.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks', 
+            image: './assets/images/socks_green.jpg',
+            url: "https://www.google.com/search?sca_esv=572ab1811f4a88fa&rlz=1C1VDKB_enGB1147GB1148&sxsrf=AE3TifMImsyRyvsWsb1mVGw2EWOtclHMXw:1753191899396&q=green+socks&udm=28&fbs=AIIjpHxU7SXXniUZfeShr2fp4giZjSkgYzz5-5RrRWAIniWd7tzPwkE1KJWcRvaH01D-XIVq8GTSVeX03zdmjcvv_uucWObceJ72YRgGjTeOjBlqVcPlJ1XQ4WOXpXA7N26QSKFxnDaTfOStme5msqlt3-fIiP8rEBw8W-9-PeDUa2VeVK4fMauAft5Y8_zBTahcRtdCELYFQxbUXwQYOuvZDD6ZvxmKRw&ved=1t:220175&ictx=111&biw=1920&bih=911&dpr=1"
         }
     }
 })
