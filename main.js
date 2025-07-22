31e782348c116156c4cb65308d47e4679806425d
Dan Clark <dannyclark_90@hotmail.co.uk>
2025-07-22 15:32:17 +0100

    Remove item from cart functionality added.

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1;
         },
+        removeFromCart() {
+            this.cart > 0 ? this.cart -= 1 : this.cart = 0;
+        },
         updateImage(variantImage) {
             this.image = variantImage;
         }
