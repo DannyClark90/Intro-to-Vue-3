7fb394b4846dd93a0c928cb407ef0c21de0d29d6
Dan Clark <dannyclark_90@hotmail.co.uk>
2025-07-24 08:48:05 +0100

    Click events emitted to add and remove items to & from cart.

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+            console.log(this.cart);
+        },
+        deleteFromCart(id) {
+            itemToRemove = this.cart.indexOf(id)
+            if (itemToRemove > -1) {
+                this.cart.splice(itemToRemove, 1)
+            }
+            console.log(`Item to remove: ${itemToRemove}`);
+            console.log(`Updated Cart: ${this.cart}`);
+        }
+    }
 })
